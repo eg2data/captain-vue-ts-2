a43c9d26b39b90b38b8debd42f5739e1df4d3739
eg2data <eg2data@gmail.com>
2023-02-07 16:43:58 +0900

    2/7 완료

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>TEST 4</h1>
+    <TodoInput :todoItem="todoText" @inputs="updateTodo" @adds="addTodo"></TodoInput>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue"
+  import TodoInput from "@/components/TodoInput.vue";
+
+  export default Vue.extend({
+    components: {TodoInput},
+    methods: {
+      updateTodo(value: string) {
+        this.todoText = value;
+      },
+      addTodo() {
+        const value = this.todoText;
+        localStorage.setItem(value, value);
+        this.initTodoText();
+      },
+      initTodoText() {
+        this.todoText = '';
+      }
+    },
+    data() {
+      return {
+        todoText: ""
+      }
+    }
+  })
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <label for="todo-item">오늘 할 일: </label>
+    <input id="todo-item" type="text" @input="handleInput" :value="todoItem">
+    <button type="button" @click="addTodo">추가</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  // props: ["todoItem"],
+  props: {
+    "todoItem": {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    addTodo() {
+      this.$emit("adds")
+    },
+    handleInput(event: InputEvent) {
+      const eventTarget = event.target as HTMLInputElement
+      this.$emit("inputs", eventTarget.value)
+
+    }
+  }
+
+})
+
+</script>
+
+<style scoped>
+
+</style>
